5b7e0e6fa2b32371f8faa6415b3181f87ddb12a1
lewisblackburn <lewisblackburn10@gmail.com>
2024-01-21 02:25:46 +0000

    added fly to fish path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting
 
 set -x PATH $HOME/.local/bin $PATH
+set -x FLYCTL_INSTALL "/home/lewisblackburn/.fly"
+set -x PATH $FLYCTL_INSTALL/bin $PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
